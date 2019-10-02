0dd02d81d9df3f2a97c3cf0af00f5f46d3048f8a
Carlos Pagoaga <cpagoaga@ryte.hn>
2019-10-02 08:45:51 -0600

    work

--- plugins/arscode-ninja-popups/assets/js/ninjapopups.drip.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).on("ninja_popups_ajax_response", function(event) {
-    if (event.response.drip !== undefined) {
-        _dcq.push(["identify", event.response.drip]);
-    }
-};
--- plugins/arscode-ninja-popups/assets/js/ninjapopups.learnq.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).on("ninja_popups_ajax_response", function(event) {
-    if (event.response.learnq !== undefined) {
-        _learnq.push(["identify", event.response.learnq]);
-    }
-};
